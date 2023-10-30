760077fac67308737600671f29ea0e2bab577191
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2023-10-30 12:29:59 +0100

    change model in invoice items

--- invoices/models.py
+++ invoices/models.py
@@ -127,6 +127,7 @@ class Group(models.Model):
     def __str__(self):
         return f"gr.{self.group}"
 
+
 class Paragraph(models.Model):
     class Meta:
         verbose_name = "Paragraf i pozycja"
@@ -139,6 +140,7 @@ class Paragraph(models.Model):
 
     def __str__(self):
         return f"{self.paragraph} - {self.name}"
+
 
 class InvoiceItems(models.Model):
     class Meta:
@@ -154,8 +156,8 @@ class InvoiceItems(models.Model):
     period_from = models.DateField(verbose_name='Okres od', null=False)
     period_to = models.DateField(verbose_name='Okres do', null=False)
     measurementSystemNumber = models.CharField(verbose_name='Nr. licznika', null=True, blank=True, max_length=15)
-    counterReading = models.DecimalField(verbose_name='Stan licznika', max_digits=10, decimal_places=2, null=True,
-                                         blank=True)
+    # counterReading = models.DecimalField(verbose_name='Stan licznika', max_digits=10, decimal_places=2, null=True,
+    counterReading = models.CharField(verbose_name='Stan licznika', max_length=80, null=True, blank=True)
     consumption = models.IntegerField(verbose_name='Zużycie', null=True, blank=True)
     unit = models.ForeignKey(to=Unit, on_delete=models.CASCADE, verbose_name='Jednostka', related_name=related_name)
     section = models.ForeignKey(to=Section, null=True, blank=True, on_delete=models.CASCADE,
