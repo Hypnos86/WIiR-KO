465e24a34b6666bd331b9fe7be8779d10e02760c
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2024-06-07 07:38:37 +0200

    change units in signal function for id - slug

--- units/signals.py
+++ units/signals.py
@@ -1,14 +1,21 @@
 from django.utils.text import slugify
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from units.models import Unit
 
 
 @receiver(pre_save, sender=Unit)
 def create_unit_fields(sender, instance, **kwargs):
-
     instance.unit_full_name = f'{instance.type.type_full} {instance.city} - {instance.address}'
     text = f'{instance.type.type_short} {instance.city} {instance.address} {instance.id}'
 
     if instance.slug != text:
         instance.slug = slugify(text, )
+
+
+@receiver(post_save, sender=Unit)
+def edit_slug(sender, instance, created, **kwargs):
+    if "none" in instance.slug:
+        text = f'{instance.type.type_short} {instance.city} {instance.address} {instance.id}'
+        instance.slug = slugify(text, )
+        instance.save()
