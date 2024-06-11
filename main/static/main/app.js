0606846542f13f5eb810fbec59689bd33a9c27bb
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2024-06-11 09:17:23 +0200

    formatted file

--- main/static/main/app.js
+++ main/static/main/app.js
@@ -1,47 +1,49 @@
+function openLoginModal(button) {
+  const url = $(button).data("url");
+  $("#login .modal-content").load(url);
+  $("#login").modal("show");
+}
 
-function openLoginModal(button){
-        const url = $(button).data("url");
-        $("#login .modal-content").load(url);
-        $("#login").modal("show");
-    }
-
-function openHelpModal(button){
-        const url = $(button).data("url");
-        $("#helpModal .modal-content").load(url);
-        $("#helpModal").modal("show");
-    }
+function openHelpModal(button) {
+  const url = $(button).data("url");
+  $("#helpModal .modal-content").load(url);
+  $("#helpModal").modal("show");
+}
 
 $(function () {
-        $('[data-toggle="info"]').tooltip()
-    })
+  $('[data-toggle="info"]').tooltip();
+});
 
 function snackbarFunction() {
-    const tost = document.getElementById("snackbar");
-    tost.className = "show";
-        setTimeout(function () {
-            tost.className = tost.className.replace("show", "");
-        }, 4000);
+  const tost = document.getElementById("snackbar");
+  tost.className = "show";
+  setTimeout(function () {
+    tost.className = tost.className.replace("show", "");
+  }, 4000);
 }
 
 function showLogInSnackbar() {
-    try{
-        const snackbar = document.getElementById("logIn");
-        snackbar.className = "log-in show";
-        setTimeout(function(){
-            snackbar.className = snackbar.className.replace("log-in show", "log-in");
-        }, 2000);
-    }
-    catch(error){
-        console.error(error)
-    }
-};
+  try {
+    const snackbar = document.getElementById("logIn");
+    snackbar.className = "log-in show";
+    setTimeout(function () {
+      snackbar.className = snackbar.className.replace("log-in show", "log-in");
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+  }
+}
 function showLogOutSnackbar() {
-    try{
-        const snackbar = document.getElementById("logOut");
-        snackbar.className = "log-out show";
-        setTimeout(function(){ snackbar.className = snackbar.className.replace("log-out show", "log-out"); }, 2000);
-    }
-    catch(error){
-        console.error(error)
-    }
-};
+  try {
+    const snackbar = document.getElementById("logOut");
+    snackbar.className = "log-out show";
+    setTimeout(function () {
+      snackbar.className = snackbar.className.replace(
+        "log-out show",
+        "log-out"
+      );
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+  }
+}
--- main/static/main/form_items.js
+++ main/static/main/form_items.js
@@ -1,129 +1,144 @@
-function initFormItems(viewsData){
-    let daneJS = "{{ measurementData|escapejs }}";
-    let x = daneJS.replace(/'/g, '"');
-    let dataObject = JSON.parse(x);
+function initFormItems(viewsData) {
+  let daneJS = "{{ measurementData|escapejs }}";
+  let x = daneJS.replace(/'/g, '"');
+  let dataObject = JSON.parse(x);
 }
 
-    paragraph.addEventListener('change', function() {
-        let selectedElement = paragraph.options[paragraph.selectedIndex];
-        let checkPar = selectedElement.textContent.substring(0, 4);
+paragraph.addEventListener("change", function () {
+  let selectedElement = paragraph.options[paragraph.selectedIndex];
+  let checkPar = selectedElement.textContent.substring(0, 4);
 
-        let measurementSystemNumber = document.getElementById("divMeasurementSystemNumber");
-        let counterReading = document.getElementById("divCounterReading");
-        let consumption = document.getElementById("divConsumption");
-        let information = document.getElementById("dataInfo");
+  let measurementSystemNumber = document.getElementById(
+    "divMeasurementSystemNumber"
+  );
+  let counterReading = document.getElementById("divCounterReading");
+  let consumption = document.getElementById("divConsumption");
+  let information = document.getElementById("dataInfo");
 
-        if (checkPar === "4260") {
+  if (checkPar === "4260") {
+    information.removeAttribute("hidden");
+    measurementSystemNumber.removeAttribute("hidden");
+    counterReading.removeAttribute("hidden");
+    consumption.removeAttribute("hidden");
+  } else if (checkPar === "4210") {
+    let labelConsumption = document.querySelector(
+      'label[for="id_consumption"]'
+    );
 
-            information.removeAttribute('hidden');
-            measurementSystemNumber.removeAttribute('hidden');
-            counterReading.removeAttribute('hidden');
-            consumption.removeAttribute('hidden');
+    if (labelConsumption) {
+      labelConsumption.textContent = "Ilość surowca";
+    }
 
-        } else if (checkPar === "4210") {
+    consumption.removeAttribute("hidden");
+    console.log(labelConsumption.value);
+    measurementSystemNumber.setAttribute("hidden", "true");
+    counterReading.setAttribute("hidden", "true");
+    information.setAttribute("hidden", "true");
+  } else {
+    measurementSystemNumber.setAttribute("hidden", "true");
+    counterReading.setAttribute("hidden", "true");
+    consumption.setAttribute("hidden", "true");
+    information.setAttribute("hidden", "true");
+  }
+});
 
-            let labelConsumption = document.querySelector('label[for="id_consumption"]');
+let unit = document.getElementById("id_unit");
+let unitSelected = unit.addEventListener("change", function () {
+  let selectedUnit = unit.value;
+  let selectedUnitext = unit.options[unit.selectedIndex].text;
+  console.log("Wybrany unit: " + selectedUnitext);
+});
 
-            if (labelConsumption){
+let type = document.getElementById("id_contract_types");
+let typeSelect = type.addEventListener("change", function () {
+  let typeText = type.options[type.selectedIndex].text;
+  console.log("Wybrany type: " + typeText);
+});
 
-                labelConsumption.textContent = "Ilość surowca";
-            };
+let paragraphSelect = paragraph.addEventListener("change", function () {
+  let selectedElement = paragraph.options[paragraph.selectedIndex];
+  let parText = selectedElement.text;
+  let checkParagraph = parText.substring(0, 7);
+  console.log("Wybrany paragraph: " + checkParagraph);
+});
 
-            consumption.removeAttribute('hidden');
-            console.log(labelConsumption.value);
-            measurementSystemNumber.setAttribute('hidden', 'true');
-            counterReading.setAttribute('hidden', 'true');
-            information.setAttribute('hidden', 'true');
+function checkData() {
+  $(dataObject).each(function (i, element) {
+    if (element.unit_id == unit.value) {
+      let elementData = element.data;
+      let information = document.getElementById("dataInfo");
 
+      for (let y = 0; y < elementData.length; y++) {
+        console.log("elementData.length: " + elementData.length);
+        console.log(
+          "wchodze do  for (let y = 0; y < elementData.length; y++ )"
+        );
+
+        console.log("elementData");
+        console.log(elementData);
+
+        if (
+          elementData[y].type === type.options[type.selectedIndex].text &&
+          elementData[y].par ===
+            paragraph.options[paragraph.selectedIndex].text.slice(0, 7)
+        ) {
+          console.log(
+            "wchodze do  if(elementData[y].type === type.options[type.selectedIndex].text && elementData[y].par === paragraph.options[paragraph.selectedIndex].text.slice(0,7)) -> True"
+          );
+          console.log("elementData[y].type");
+          console.log(elementData[y].type);
+          console.log("type.options[type.selectedIndex].text");
+          console.log(type.options[type.selectedIndex].text);
+          console.log(
+            elementData[y].type === type.options[type.selectedIndex].text
+          );
+
+          console.log("elementData[y].par");
+          console.log(elementData[y].par);
+          console.log(
+            "paragraph.options[paragraph.selectedIndex].text.slice(0,7)"
+          );
+          console.log(
+            paragraph.options[paragraph.selectedIndex].text.slice(0, 7)
+          );
+          console.log(
+            elementData[y].par ===
+              paragraph.options[paragraph.selectedIndex].text.slice(0, 7)
+          );
+
+          let measureData = document.getElementById(
+            "id_measurementSystemNumber"
+          );
+          console.log("measureData");
+          console.log(measureData);
+
+          if (measureData) {
+            information.removeAttribute("hidden");
+            information.textContent =
+              " Ostatni okres rozliczeniowy: " +
+              elementData[y].period +
+              ", " +
+              "stan licznika:" +
+              elementData[y].counterReading;
+            measureData.value = elementData[y].measurement;
+          }
         } else {
-            measurementSystemNumber.setAttribute('hidden', 'true');
-            counterReading.setAttribute('hidden', 'true');
-            consumption.setAttribute('hidden', 'true');
-            information.setAttribute('hidden', 'true');
+          let measureData = document.getElementById(
+            "id_measurementSystemNumber"
+          );
+          measureData.value = "";
+          information.setAttribute("hidden", "true");
         }
-    });
+      }
+    } else {
+      console.log("wchodze do if( element.unit_id == unit.value) -> False");
+    }
+  });
+}
+$("#id_contract_types").on("change", function () {
+  checkData();
+});
 
-
-    let unit = document.getElementById("id_unit");
-    let unitSelected = unit.addEventListener('change', function() {
-        let selectedUnit = unit.value;
-        let selectedUnitext = unit.options[unit.selectedIndex].text;
-        console.log("Wybrany unit: " + selectedUnitext);
-    });
-
-    let type = document.getElementById("id_contract_types");
-    let typeSelect = type.addEventListener('change', function() {
-        let typeText = type.options[type.selectedIndex].text;
-        console.log("Wybrany type: " + typeText);
-    });
-
-    let paragraphSelect = paragraph.addEventListener('change', function() {
-        let selectedElement = paragraph.options[paragraph.selectedIndex];
-        let parText = selectedElement.text;
-        let checkParagraph = parText.substring(0, 7);
-        console.log("Wybrany paragraph: " + checkParagraph);
-    });
-
-    function checkData(){
-        $(dataObject).each(function(i, element) {
-            if( element.unit_id == unit.value) {
-
-                let elementData = element.data;
-                let information = document.getElementById("dataInfo");
-
-                for (let y = 0; y < elementData.length; y++ ){
-                    console.log("elementData.length: "+elementData.length);
-                    console.log("wchodze do  for (let y = 0; y < elementData.length; y++ )");
-
-                    console.log("elementData");
-                    console.log(elementData);
-
-                    if(elementData[y].type === type.options[type.selectedIndex].text && elementData[y].par === paragraph.options[paragraph.selectedIndex].text.slice(0,7)){
-
-                        console.log("wchodze do  if(elementData[y].type === type.options[type.selectedIndex].text && elementData[y].par === paragraph.options[paragraph.selectedIndex].text.slice(0,7)) -> True");
-                        console.log("elementData[y].type");
-                        console.log(elementData[y].type);
-                        console.log("type.options[type.selectedIndex].text");
-                        console.log(type.options[type.selectedIndex].text);
-                        console.log(elementData[y].type === type.options[type.selectedIndex].text);
-
-                        console.log("elementData[y].par");
-                        console.log(elementData[y].par);
-                        console.log("paragraph.options[paragraph.selectedIndex].text.slice(0,7)");
-                        console.log(paragraph.options[paragraph.selectedIndex].text.slice(0,7));
-                        console.log(elementData[y].par === paragraph.options[paragraph.selectedIndex].text.slice(0,7));
-
-                        let measureData = document.getElementById("id_measurementSystemNumber");
-                        console.log("measureData");
-                        console.log(measureData);
-
-                        if(measureData){
-
-                        information.removeAttribute('hidden');
-                        information.textContent = " Ostatni okres rozliczeniowy: " + elementData[y].period + ", " + "stan licznika:"+ elementData[y].counterReading;
-                        measureData.value = elementData[y].measurement;
-
-                        };
-
-                    }else{
-
-                    console.log("wchodze do else");
-
-                    let measureData = document.getElementById("id_measurementSystemNumber");
-                    measureData.value = "";
-                    information.setAttribute('hidden', 'true');
-
-                    };
-                };
-            }else{
-                console.log("wchodze do if( element.unit_id == unit.value) -> False");
-            };
-        });
-    };
-    $('#id_contract_types').on('change', function(){
-        checkData();
-    });
-
-    $('#id_paragraph').on('change', function(){
-        checkData();
-    });
+$("#id_paragraph").on("change", function () {
+  checkData();
+});
