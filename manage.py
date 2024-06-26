57789b70bdb630a216ca6f3d71e7b92d4dc1c0b6
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2024-06-26 14:18:15 +0200

    change settings, clear code

--- manage.py
+++ manage.py
@@ -15,9 +15,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    print('len(sys.argv) = ', len(sys.argv))
-    if len(sys.argv) == 1:
-        sys.argv += ['runserver 10.15.0.15:8000']
     execute_from_command_line(sys.argv)
 
 
--- static/main/app.js
+++ static/main/app.js
@@ -1,32 +1,49 @@
-
-function openLoginModal(button){
-        var url = $(button).data("url");
-        $("#login .modal-content").load(url);
-        $("#login").modal("show");
-    }
-
-function openHelpModal(button){
-        var url = $(button).data("url");
-        $("#helpModal .modal-content").load(url);
-        $("#helpModal").modal("show");
-    }
-
-$(function () {
-        $('[data-toggle="info"]').tooltip()
-    })
-
-function snackbarFunction() {
-    var tost = document.getElementById("snackbar");
-    tost.className = "show";
-        setTimeout(function () {
-            tost.className = tost.className.replace("show", "");
-        }, 10000);
+function openLoginModal(button) {
+  const url = $(button).data("url");
+  $("#login .modal-content").load(url);
+  $("#login").modal("show");
 }
 
-function snackbarLogOutFunction() {
-    var tost = document.getElementById("logOut");
-    tost.className = "show";
-        setTimeout(function () {
-            tost.className = tost.className.replace("show", "");
-        }, 10000);
+function openHelpModal(button) {
+  const url = $(button).data("url");
+  $("#helpModal .modal-content").load(url);
+  $("#helpModal").modal("show");
+}
+
+$(function () {
+  $('[data-toggle="info"]').tooltip();
+});
+
+function snackbarFunction() {
+  const tost = document.getElementById("snackbar");
+  tost.className = "show";
+  setTimeout(function () {
+    tost.className = tost.className.replace("show", "");
+  }, 4000);
+}
+
+function showLogInSnackbar() {
+  try {
+    const snackbar = document.getElementById("logIn");
+    snackbar.className = "log-in show";
+    setTimeout(function () {
+      snackbar.className = snackbar.className.replace("log-in show", "log-in");
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+  }
+}
+function showLogOutSnackbar() {
+  try {
+    const snackbar = document.getElementById("logOut");
+    snackbar.className = "log-out show";
+    setTimeout(function () {
+      snackbar.className = snackbar.className.replace(
+        "log-out show",
+        "log-out"
+      );
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+  }
 }
